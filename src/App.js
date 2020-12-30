98b8ed5557907dc5b03a9b3e8136ea47ab4ec3a8
Wes <wesleySalesberry@hotmail.com>
2020-12-29 21:08:09 -0500

    the src file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import { NavBar } from './components/Nav/NavBar';
+import { AllPost } from './pages/AllPost/AllPost';
+import { CreatePost } from './pages/Post/CreatePost';
+import { Display } from './pages/DisplayBoastRoast/Display';
+import { Sorted } from './pages/SortedPost/Sorted'
+import { Remove } from './pages/RemovePost/Remove';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={AllPost} />
+        <Route exact path="/post" component={CreatePost}/>
+        <Route exact path="/display" component={Display}/>
+        <Route exact path="/sorted" component={Sorted} />
+        <Route exact path="/remove/:magicstring" component={Remove}/>
+      </Switch>
+    </Router>
   );
 }
 
